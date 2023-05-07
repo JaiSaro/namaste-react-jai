212c9c917a419e75e93e84c85755fdaaf6f949ef
Jai Saravanan <40114480+JaiSaro@users.noreply.github.com>
2023-05-07 11:18:21 +0530

    Episode1

---
+++ inception/App.js
@@ -0,0 +1,7 @@
+const heading = React.createElement(
+    "h1",
+    { id: "header-one", class: "c2" },
+    "Hello World from react!"
+);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
